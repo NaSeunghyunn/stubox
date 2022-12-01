24bf21bbf5c1da735e3078c3e6ec5e660e2d5165
SeungHyunNa <nasbong077@gmail.com>
2022-12-01 17:45:35 +0900

    부트스트랩 화면 개선

--- src/main/java/com/nastudy/stubox/config/SecurityConfig.java
+++ src/main/java/com/nastudy/stubox/config/SecurityConfig.java
@@ -26,7 +26,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf().disable();
         http.authorizeRequests()
-                .antMatchers("/auth/**").permitAll()
+                .antMatchers("/auth/**","/img/**","/js/**").permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
