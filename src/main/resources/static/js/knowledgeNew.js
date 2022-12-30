baf3e5b5796d372502145e940dfb902ade1df8a5
SeungHyunNa <nasbong077@gmail.com>
2022-12-30 23:12:27 +0900

    인덴트

--- src/main/resources/static/js/knowledgeNew.js
+++ src/main/resources/static/js/knowledgeNew.js
@@ -1,20 +1,20 @@
-$(document).ready(function(){
+$(document).ready(function () {
     var timeout = null;
-    $("#tag-input").keyup(function(){
+    $("#tag-input").keyup(function () {
         clearTimeout(timeout);
         timeout = setTimeout(function () {
-            if(!$("#tag-input").val()){
-                        clearSearchTag();
-                        return;
-                    }
+            if (!$("#tag-input").val()) {
+                clearSearchTag();
+                return;
+            }
             api.getTags();
         }, 500);
     });
 
-    $("#tag-sel").change(function(){
+    $("#tag-sel").change(function () {
         let addFlag = addTagFlag();
 
-        if(addFlag){
+        if (addFlag) {
             addTag();
             api.saveTag();
         }
@@ -24,32 +24,32 @@ $(document).ready(function(){
 });
 
 let api = {
-    getTags: function(){
+    getTags: function () {
         let url = "/tag";
         let params = {
             name: $("#tag-input").val()
         }
         commonMethod.fetchGet(url, params)
-        .then(data => this.getTagsCallback(data))
-        .catch(err => false);
+            .then(data => this.getTagsCallback(data))
+            .catch(err => false);
     },
 
-    getTagsCallback: function(data){
-        let size = data.length < 5? data.length : 4 ;
-        if(size == 1) size++;
+    getTagsCallback: function (data) {
+        let size = data.length < 5 ? data.length : 4;
+        if (size == 1) size++;
         $("#tag-sel").attr("size", size);
         $("#tag-sel").empty();
         $("#tag-sel").show(300);
-        $(data).each(function(index, item){
+        $(data).each(function (index, item) {
             let id = item.id == null ? "" : item.id;
-            let option = "<option value='"+id+"'>"+item.name+"</option>"
+            let option = "<option value='" + id + "'>" + item.name + "</option>"
             $("#tag-sel").append(option);
         });
     },
 
-    saveTag: function(){
+    saveTag: function () {
         let tagId = $("#tag-sel option:selected").val();
-        if(tagId){
+        if (tagId) {
             return;
         }
 
@@ -59,14 +59,14 @@ let api = {
         }
 
         commonMethod.fetchPost(url, body)
-        .catch(err => false);
+            .catch(err => false);
     },
 
-    saveKnowledge: function(){
-        let url= "/knowledge";
+    saveKnowledge: function () {
+        let url = "/knowledge";
         let $tags = $(".tag-id");
         let tagIds = [];
-        $tags.each(function(index, item){
+        $tags.each(function (index, item) {
             tagIds.push($(item).val());
         });
         let body = {
@@ -76,77 +76,77 @@ let api = {
             tagIds: tagIds
         }
         commonMethod.fetchPost(url, body)
-                .catch(err => false);
+            .catch(err => false);
     }
 }
 
 $('.summernote').summernote({
-	height: 300,
-	lang: "ko-KR",
-	callbacks: {
-		onImageUpload: function (files, editor, welEditable) {
-			// 파일 업로드(다중업로드를 위해 반복문 사용)
-			for (var i = files.length - 1; i >= 0; i--) {
-				uploadSummernoteImageFile(files[i], this);
-			}
-		}
-	}
+    height: 300,
+    lang: "ko-KR",
+    callbacks: {
+        onImageUpload: function (files, editor, welEditable) {
+            // 파일 업로드(다중업로드를 위해 반복문 사용)
+            for (var i = files.length - 1; i >= 0; i--) {
+                uploadSummernoteImageFile(files[i], this);
+            }
+        }
+    }
 });
 
 function uploadSummernoteImageFile(file, el) {
-	let formData = new FormData();
-	formData.append("file", file);
-	let url = "/knowledge/image";
-	$(".loading").show();
-	commonMethod.fetchFormData(url, "POST", formData)
+    let formData = new FormData();
+    formData.append("file", file);
+    let url = "/knowledge/image";
+    $(".loading").show();
+    commonMethod.fetchFormData(url, "POST", formData)
         .then((data) => $(el).summernote('editor.insertImage', data.url))
         .catch(err => false)
-        .finally(()=>$(".loading").hide());
+        .finally(() => $(".loading").hide());
 }
 
-function removeTag(tag){
+function removeTag(tag) {
     let $tagDiv = $(tag).parent(".tag");
     $tagDiv.remove();
 }
 
-function addTag(){
+function addTag() {
     let tagDiv = document.createElement('div');
-            let $tagDiv = $(tagDiv);
-            $tagDiv.attr("class","tag m-1");
+    let $tagDiv = $(tagDiv);
+    $tagDiv.attr("class", "tag m-1");
 
-            let tagName = document.createElement('div');
-            let $tagName = $(tagName);
-            $tagName.attr("class", "tag-name");
-            $tagName.text($("#tag-sel option:selected").text());
+    let tagName = document.createElement('div');
+    let $tagName = $(tagName);
+    $tagName.attr("class", "tag-name");
+    $tagName.text($("#tag-sel option:selected").text());
 
-            let tagId = document.createElement('input');
-            let $tagId = $(tagId);
-            $tagId.attr("type", "hidden");
-            $tagId.attr("class", "tag-id");
-            $tagId.val($("#tag-sel option:selected").val());
+    let tagId = document.createElement('input');
+    let $tagId = $(tagId);
+    $tagId.attr("type", "hidden");
+    $tagId.attr("class", "tag-id");
+    $tagId.val($("#tag-sel option:selected").val());
 
-            let button = document.createElement('button');
-            let $button = $(button);
-            $button.attr("onclick", "removeTag(this)");
-            $button.text("×");
+    let button = document.createElement('button');
+    let $button = $(button);
+    $button.attr("onclick", "removeTag(this)");
+    $button.text("×");
 
-            $tagDiv.append($tagName).append($tagId).append($button);
-            $(".tags").append($tagDiv);
+    $tagDiv.append($tagName).append($tagId).append($button);
+    $(".tags").append($tagDiv);
 }
 
-function addTagFlag(){
+function addTagFlag() {
     let flg = true;
-    $(".tag-name").each(function(index, item){
-                if($("#tag-sel option:selected").text() === $(item).text()){
-                flg = false;
-                    return false;
-                }
-            });
-            return flg;
+    $(".tag-name").each(function (index, item) {
+        if ($("#tag-sel option:selected").text() === $(item).text()) {
+            flg = false;
+            return false;
+        }
+    });
+    return flg;
 }
 
-function clearSearchTag(){
-        $("#tag-input").val("");
-        $("#tag-sel").hide();
-        $("#tag-sel").empty();
+function clearSearchTag() {
+    $("#tag-input").val("");
+    $("#tag-sel").hide();
+    $("#tag-sel").empty();
 }
